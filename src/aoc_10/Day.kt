5b3605c996a2f9a33881f93fd4b5d596a61adb92
GAcs <Geza.Acs@heitec.hu>
2021-12-10 19:48:13 +0100

    aoc_9 refactored with BorderedTable.kt utility class

--- src/aoc_10/Day.kt
+++ src/aoc_10/Day.kt
@@ -2,14 +2,46 @@ package aoc_10
 
 import RunAoc
 import utils.readInput
+import java.math.BigInteger
 
-class Day :RunAoc{
+data class Brackets(val ch: Char, var open: Int, var close: Int)
+class Day : RunAoc {
     val input = ArrayList<String>(readInput(10))
+    val points = hashMapOf<Char, Int>(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
+    val pointsB = hashMapOf<Char, Int>(')' to 1, ']' to 2, '}' to 3, '>' to 4)
+    val reversed = hashMapOf<Char, Char>(')' to '(', ']' to '[', '}' to '{', '>' to '<')
+    val opened = reversed.entries.associate { (k, v) -> v to k }
+
     override fun a(): Int {
+        return input.map { getScoreInLine(it).second }.sum()
+    }
+
+    override fun b(): Int {
+        val inComp = input.map { getScoreInLine(it).first }.filter { it.isNotEmpty() }
+        val scores=inComp.map { val rev=it
+            var score:BigInteger=BigInteger.ZERO
+            while(rev.isNotEmpty()) {
+                val c = rev.removeLast()
+                score=score* BigInteger.valueOf(5)
+                score += pointsB.getOrDefault(opened.getValue(c), 0).toBigInteger()
+            }
+            score
+        }.sorted()
+        println(scores[scores.size/2])
         return 0
     }
 
-    override fun b():Int {
-        return 0
+    private fun getScoreInLine(str: String): Pair<MutableList<Char>, Int> {
+        val lsOpen = emptyList<Char>().toMutableList()
+        str.forEach {
+            if (it in opened.keys) {
+                lsOpen.add(it)
+            } else {
+                if (lsOpen.last() == reversed.getValue(it)) lsOpen.removeLast()
+                else
+                    return emptyList<Char>().toMutableList() to points.getValue(it)
+            }
+        }
+        return lsOpen to 0
     }
 }
